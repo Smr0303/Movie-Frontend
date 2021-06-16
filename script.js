280d854fce86b451719528b476e8d64b63141a1b
Your Name <you@example.com>
2021-06-16 11:41:35 +0530

    grid->flex

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const body=document.querySelector("body");
 window.addEventListener('load',()=>{
 body.classList.add("visible");
 });
-
 const slider = document.querySelector(".slider");
 const left_arrow = document.querySelector(".left_arrow");
 const right_arrow = document.querySelector(".right_arrow");
