4663b985bc3c50ef0f6d0253783525c46fdee9bd
Your Name <you@example.com>
2021-06-16 11:36:06 +0530

    grid->flex

--- script.js
+++ script.js
@@ -69,3 +69,58 @@ setInterval(() => {
     j= (j+1)%str.length;
 }, 200);
 }
+
+let streaming = document.querySelector(".streaming");
+
+fetch(`https://api.themoviedb.org/3/list/7099202?api_key=65bdc6e47dd2725b55a936c4b0242e7b&language=en-US`).then((res)=> {
+    return res.json()
+}).then((data) => {
+    console.log(data)
+    for(var i=0;i<data.items.length;i++)
+    {
+        let card = document.createElement("div");
+        card.classList.add("card")
+        card.innerHTML = `
+        <div class="card-motion">
+   <div class="movie_container">
+       <img src="https://image.tmdb.org/t/p/w200/${data.items[i].poster_path}" alt="">
+       <div class="title">
+         <h1>${data.items[i].title}</h1>
+        </div>
+         <div class="rating">
+           <img src="./assets/star.png" alt="">
+           <h2>${data.items[i].vote_average}</h2>
+         </div>
+
+    </div>
+    <div class="card-back">
+      <p>${data.items[i].overview}</p>
+    </div>
+  </div> `;
+        streaming.appendChild(card)
+    }
+})
+
+
+
+
+// fetch(`https://api.themoviedb.org/3/discover/movie?api_key=65bdc6e47dd2725b55a936c4b0242e7b&language=en-US&sort_by=popularity.desc`).then((res)=> {
+//     return res.json()
+// }).then((data) => {
+//     console.log(data)
+//     for(var i=0;i<data.results.length;i++)
+//     {
+//         let movie_container = document.createElement("div");
+//         movie_container.classList.add("movie_container")
+//         movie_container.innerHTML = `
+//         <img src="https://image.tmdb.org/t/p/w200${data.results[i].poster_path}" alt="">
+//         <div class="title">
+//           <h1>${data.results[i].title}"</h1>
+//           </div>
+//           <div class="rating">
+//             <img src="./assets/star.png" alt="">
+//             <h2>${data.results[i].vote_average}"</h2>
+//           </div>`;
+//         streaming.appendChild(movie_container)
+//     }
+// })
